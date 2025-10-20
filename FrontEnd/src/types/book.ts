a65a2f542f0cfafed661849b4f19a67150fcdf80
Nam Hoàng <151829232+hoangnam140205@users.noreply.github.com>
2025-10-20 13:14:16 +0700

    Create book.ts

---
+++ FrontEnd/src/types/book.ts
@@ -0,0 +1,11 @@
+export type Book = {
+  id: number
+  title: string
+  author: string
+  year: number
+  stock: number
+  available: number
+  category: string
+  code: string
+  description?: string
+}
